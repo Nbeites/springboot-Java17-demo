654483981c5aa5b713134b32b1e747eb9b26c3d9
nbeites <nm.beites@gmail.com>
2024-10-15 22:06:12 +0100

    first commit - SampleJava 17 Springboot

---
+++ src/main/java/com/demo/springboot/SpringbootJava17Application.java
@@ -0,0 +1,13 @@
+package com.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJava17Application.class, args);
+	}
+
+}
